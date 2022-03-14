3a0efeb7f6eef13d02d1e938b4c7048f8b42c5db
harshvardhan singh <harshv251@gmail.com>
2022-03-14 16:33:19 +0530

    th product compare plugin issue fixed

--- inc/woocommerce/woo-function.php
+++ inc/woocommerce/woo-function.php
@@ -275,6 +275,7 @@ remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_l
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 
 
+
 /***************/
 // single page
 /***************/
